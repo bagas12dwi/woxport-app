a5164fed49c0e40d0883931ec1f327e8d068e22b
bagas12dwi <bag12dwi.s@gmail.com>
2023-11-21 09:28:07 +0700

    added middleware routing

--- routes/web.php
+++ routes/web.php
@@ -44,53 +44,66 @@ Route::get('/contact', function () {
         'title' => 'Contact Us'
     ]);
 });
-Route::get('/change-password', function () {
-    return view('users.change-password');
-});
-Route::get('/success', function () {
-    return view('users.success-payment', [
-        'title' => 'Success'
-    ]);
-});
 Route::get('/product', [UserProductController::class, 'index']);
 Route::get('/blog', [UserBlogController::class, 'index']);
 Route::get('/blog/detail/{blog_id}', [UserBlogController::class, 'detail']);
 Route::get('/list_vendor/{category_id}', [VendorController::class, 'index']);
 Route::get('/vendor/detail/{product_id}', [VendorController::class, 'detail']);
-Route::resource('/wishlist', WishlistController::class);
-Route::delete('/unlike/{wishlist}', [WishlistController::class, 'unlike']);
-Route::resource('/cart', CartController::class);
-Route::resource('vendor/daftar-toko', ControllersVendorController::class);
-Route::post('/payment/store', [PaymentController::class, 'store']);
-Route::get('/payment/{order_number}', [PaymentController::class, 'index']);
-Route::put('/payment/{order_number}', [PaymentController::class, 'update']);
-Route::get('/profile', [ProfileController::class, 'index']);
-Route::put('/profile/{user}', [ProfileController::class, 'update']);
-Route::get('/daftar-transaksi', [TransactionListController::class, 'index']);
-Route::get('/daftar-transaksi/detail/{order_number}', [TransactionListController::class, 'detail']);
+
+// ROUTE USER 
+Route::group(['middleware' => ['auth', 'cekrole:user,vendor']], function () {
+    Route::get('/success', function () {
+        return view('users.success-payment', [
+            'title' => 'Success'
+        ]);
+    });
+    Route::resource('/wishlist', WishlistController::class);
+    Route::delete('/unlike/{wishlist}', [WishlistController::class, 'unlike']);
+    Route::resource('/cart', CartController::class);
+    Route::resource('vendor/daftar-toko', ControllersVendorController::class);
+    Route::post('/payment/store', [PaymentController::class, 'store']);
+    Route::get('/payment/{order_number}', [PaymentController::class, 'index']);
+    Route::put('/payment/{order_number}', [PaymentController::class, 'update']);
+    Route::get('/profile', [ProfileController::class, 'index']);
+    Route::put('/profile/{user}', [ProfileController::class, 'update']);
+    Route::get('/daftar-transaksi', [TransactionListController::class, 'index']);
+    Route::get('/daftar-transaksi/detail/{order_number}', [TransactionListController::class, 'detail']);
+});
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/change-password', function () {
+        return view('users.change-password');
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 
 // Route Auth
-Route::get('/login', [AuthController::class, 'indexLogin']);
-Route::get('/register', [AuthController::class, 'indexRegister']);
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::group(['middleware' => ['guest']], function () {
+    Route::get('/login', [AuthController::class, 'indexLogin']);
+    Route::get('/register', [AuthController::class, 'indexRegister']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
 
 // ROUTE VENDOR 
-Route::resource('/vendor/produk', ProductController::class);
-Route::get('/vendor/toko', [ShopController::class, 'index']);
-Route::get('/vendor/edit-toko', [ShopController::class, 'edit']);
-Route::post('/vendor/edit-toko', [ShopController::class, 'update']);
-Route::get('/vendor/transaksi', [TransactionController::class, 'index']);
-Route::get('/vendor/transaksi/konfirmasi', [TransactionController::class, 'indexConfirmTransaction']);
-Route::get('/vendor/transaksi/detail/{order_number}', [TransactionController::class, 'detail']);
-Route::post('/vendor/transaksi/konfirmasi/setuju', [TransactionController::class, 'konfirmasi']);
-Route::post('/vendor/transaksi/konfirmasi/tolak', [TransactionController::class, 'tolak']);
+Route::group(['middleware' => ['auth', 'cekrole:vendor']], function () {
+    Route::resource('/vendor/produk', ProductController::class);
+    Route::get('/vendor/toko', [ShopController::class, 'index']);
+    Route::get('/vendor/edit-toko', [ShopController::class, 'edit']);
+    Route::post('/vendor/edit-toko', [ShopController::class, 'update']);
+    Route::get('/vendor/transaksi', [TransactionController::class, 'index']);
+    Route::get('/vendor/transaksi/konfirmasi', [TransactionController::class, 'indexConfirmTransaction']);
+    Route::get('/vendor/transaksi/detail/{order_number}', [TransactionController::class, 'detail']);
+    Route::post('/vendor/transaksi/konfirmasi/setuju', [TransactionController::class, 'konfirmasi']);
+    Route::post('/vendor/transaksi/konfirmasi/tolak', [TransactionController::class, 'tolak']);
+});
 
 // ROUTE ADMIN
-Route::get('/dashboard', [DashboardController::class, 'index']);
-Route::resource('kategori', CategoryController::class);
-Route::resource('user', UserController::class);
-Route::resource('manage-blog', BlogController::class);
-Route::resource('manage-bank', BankAccountController::class);
+Route::group(['middleware' => ['auth', 'cekrole:admin']], function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::resource('kategori', CategoryController::class);
+    Route::resource('user', UserController::class);
+    Route::resource('manage-blog', BlogController::class);
+    Route::resource('manage-bank', BankAccountController::class);
+});
